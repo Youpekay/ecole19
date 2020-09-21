3b9920b2b713d8c563d8d154029a2082206852fe
Matthieu Reniere <mreniere@shi-r2-p1.s19.be>
2020-09-21 19:12:18 +0200

    ok

--- C06/ex02/ft_rev_params.c
+++ C06/ex02/ft_rev_params.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mreniere <mreniere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/09 18:11:38 by mreniere          #+#    #+#             */
-/*   Updated: 2020/09/21 14:31:09 by mreniere         ###   ########.fr       */
+/*   Created: 2020/09/21 08:34:38 by mreniere          #+#    #+#             */
+/*   Updated: 2020/09/21 08:36:46 by mreniere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,22 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-char *str1;
-char c;
-
-str = argv[2];
-c = str[0];
-
-argv[2][0]
-
-argv[0] = name of the program (like "./a.out")
-argv[1] = first parameter
-argv[2] = second parameter
-
-
-./a.out "sdfdfgdgdsfghs"
-
-
-if(argc != 2)
+void	ft_putstr(char *str)
 {
-	write();
+	while (*str)
+		ft_putchar(*str++);
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[argc - i]);
+		ft_putchar('\n');
+		i++;
+	}
 	return (0);
 }
---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mreniere <mreniere@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/21 08:38:22 by mreniere          #+#    #+#             */
+/*   Updated: 2020/09/21 19:11:05 by mreniere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	char c;
+
+	while (*str != '\0')
+	{
+		c = *str;
+		ft_putchar(c);
+		str++;
+	}
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	if (s1[i] < s2[i])
+		return (1);
+	else
+		return (0);
+}
+
+int		main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*tmp;
+
+	i = 0;
+	while (++i < argc)
+	{
+		j = 0;
+		while (++j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]))
+			{
+				tmp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = tmp;
+			}
+		}
+	}
+	i = 0;
+	while (++i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putstr("\n");
+	}
+	return (0);
+}
