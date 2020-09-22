7e0fa4ab6d96c9fdf2d6ec29b3829384d977987e
Matthieu Reniere <mreniere@shi-r2-p1.s19.be>
2020-09-22 12:28:30 +0200

    ok

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -6,15 +6,17 @@
 /*   By: mreniere <mreniere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/22 10:07:28 by mreniere          #+#    #+#             */
-/*   Updated: 2020/09/22 10:42:25 by mreniere         ###   ########.fr       */
+/*   Updated: 2020/09/22 12:27:55 by mreniere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
 
 int		ft_strlen(char *str)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	while (str[i])
 		i++;
 	return (i);
@@ -24,6 +26,7 @@ void	ft_strcpy(char *dest, char *src)
 {
 	int i;
 
+	i = 0;
 	while (src[i])
 	{
 		dest[i] = src[i];
@@ -46,22 +49,34 @@ int		ft_calc_total_size(int size, char **strs, char *sep)
 		i++;
 	}
 	total_size -= ft_strlen(sep);
-
 	return (total_size);
 }
 
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
-	int i;
-	int total_size;
-	char *empty;
+	int		i;
+	char	*empty;
+	char	*result;
+	char	*result_start;
 
-	i = 0;
-	if(!size)
-	{
-		empty = malloc(sizeof(char));
+	i = -1;
+	empty = malloc(sizeof(char));
+	if (!size)
 		return (empty);
+	if (!(result = malloc((ft_calc_total_size(size, strs, sep)
+		+ 1) * sizeof(char))))
+		return (0);
+	result_start = result;
+	while (++i < size)
+	{
+		ft_strcpy(result, strs[i]);
+		result += ft_strlen(strs[i]);
+		if (i < size - 1)
+		{
+			ft_strcpy(result, sep);
+			result += ft_strlen(sep);
+		}
 	}
-	total_size = ft_calc_total_size(size, strs, sep);
-	if(!)
+	*result = '\0';
+	return (result_start);
 }
