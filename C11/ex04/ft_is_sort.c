4f41844c6bae3b55865480e2f8e1309a85adeee9
Matthieu Reniere <mreniere@shi-r2-p1.s19.be>
2020-09-28 20:42:00 +0200

    ok

--- C11/ex04/ft_is_sort.c
+++ C11/ex04/ft_is_sort.c
@@ -6,11 +6,28 @@
 /*   By: mreniere <mreniere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 17:27:50 by mreniere          #+#    #+#             */
-/*   Updated: 2020/09/28 17:33:31 by mreniere         ###   ########.fr       */
+/*   Updated: 2020/09/28 20:41:20 by mreniere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_is_sort(int *tab, int length, int(*f)(int, int))
+#include <stdlib.h>
+#include <stdio.h>
+
+int		ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
-	
+	int i;
+	int r1;
+	int r2;
+
+	i = 1;
+	while (i < length && !(r1 = (*f)(tab[i - 1], tab[i])))
+		i++;
+	while (i < length)
+	{
+		r2 = (*f)(tab[i - 1], tab[i]);
+		if ((r1 < 0 && r2 > 0) || (r1 > 0 && r2 < 0))
+			return (0);
+		i++;
+	}
+	return (1);
 }
