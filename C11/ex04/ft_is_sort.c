a2c2cce1dc0dd8d5c39f9221e8158b70473afe73
Matthieu Reniere <mreniere@shi-r2-p1.s19.be>
2020-09-28 17:39:29 +0200

    ok

--- C11/ex04/ft_is_sort.c
+++ C11/ex04/ft_is_sort.c
@@ -1,29 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_count_if.c                                      :+:      :+:    :+:   */
+/*   ft_is_sort.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mreniere <mreniere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/28 15:28:51 by mreniere          #+#    #+#             */
-/*   Updated: 2020/09/28 17:25:09 by mreniere         ###   ########.fr       */
+/*   Created: 2020/09/28 17:27:50 by mreniere          #+#    #+#             */
+/*   Updated: 2020/09/28 17:33:31 by mreniere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_count_if(char **tab, int length, int (*f)(char*))
+int		ft_is_sort(int *tab, int length, int(*f)(int, int))
 {
-	int	i;
-	int	res;
-	int	count;
-
-	i = 0;
-	count = 0;
-	while (i < length)
-	{
-		res = (*f)(tab[i]);
-		if (res != 0)
-			count++;
-		i++;
-	}
-	return (count);
+	
 }
