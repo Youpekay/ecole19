e39fb357aa0662ed9ace21739bfc44ae9f05cd81
Matthieu Reniere <mreniere@shi-r2-p1.s19.be>
2020-09-28 19:22:14 +0200

    ok

--- C09/ex00/ft_strcmp.c
+++ C09/ex00/ft_strcmp.c
@@ -3,39 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mreniere <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mreniere <mreniere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 09:39:08 by mreniere          #+#    #+#             */
-/*   Updated: 2020/09/14 17:05:42 by mreniere         ###   ########.fr       */
+/*   Updated: 2020/09/28 19:20:09 by mreniere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strlen(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
 int		ft_strcmp(char *s1, char *s2)
 {
-	int real_size;
-	int i;
+	int		i;
 
 	i = 0;
-	real_size = ft_strlen(s1) >= ft_strlen(s2) ? ft_strlen(s1) : ft_strlen(s2);
-	while (real_size-- > 0)
-	{
-		if (s1[i] > s2[i])
-			return (s1[i] - s2[i]);
-		else if (s1[i] < s2[i])
-			return ((s2[i] - s1[i]) * -1);
+	while (s1[i] == s2[i] && s1[i] & s2[i])
 		i++;
-	}
-	return (0);
+	return (s1[i] - s2[i]);
 }
