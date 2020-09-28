29b999a0b8a4dc20476ca71a7c0009995fb05473
Matthieu Reniere <mreniere@shi-r2-p1.s19.be>
2020-09-28 16:47:29 +0200

    ok

--- C11/ex03/ft_count_if.c
+++ C11/ex03/ft_count_if.c
@@ -6,12 +6,24 @@
 /*   By: mreniere <mreniere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 15:28:51 by mreniere          #+#    #+#             */
-/*   Updated: 2020/09/28 15:34:39 by mreniere         ###   ########.fr       */
+/*   Updated: 2020/09/28 16:42:24 by mreniere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_count_if(char **tab, int length, int (*f)(char*))
 {
-	
-	
+	int	i;
+	int	mod;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (i < length)
+	{
+		mod = (*f)(tab[i]);
+		if (mod != 0)
+			count++;
+		i++;
+	}
+	return (count);
 }
