94ee8e9a3e2ceb53c3fbf8035c50bc06b851ba47
Youpekay <youpekay@gmail.com>
2020-11-18 01:09:11 +0100

    ok

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,29 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mreniere <mreniere@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/17 12:32:37 by mreniere          #+#    #+#             */
-/*   Updated: 2020/11/18 01:00:31 by mreniere         ###   ########.fr       */
+/*   Created: 2020/11/18 00:59:29 by mreniere          #+#    #+#             */
+/*   Updated: 2020/11/18 01:01:56 by mreniere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
+#include "libft.h"
 
-# define LIBFT_H
-# include <string.h>
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char *d;
+	unsigned char *s;
 
-size_t		ft_strlen(const char *s);
-int			ft_islower(int c);
-int			ft_isupper(int c);
-int			ft_isalpha(int c);
-
-void		*ft_memset(void *s, int c, size_t n);
-void		ft_bzero(void *s, size_t n);
-
-void *memcpy(void *dest, const void *src, size_t n);
-
-
-#endif
+	if (!dest && !src)
+		return (NULL);
+	d = (unsigned char*)dest;
+	s = (unsigned char*)src;
+	while (n--)
+		*(d++) = *(s++);
+	return (dest);
+}
