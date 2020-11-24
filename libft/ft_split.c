8ce6ea3a741a85af31e032530a3195aed1d1c7a5
Youpekay <youpekay@gmail.com>
2020-11-24 16:09:08 +0100

    ok

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,81 +6,77 @@
 /*   By: mreniere <mreniere@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 04:59:11 by mreniere          #+#    #+#             */
-/*   Updated: 2020/11/24 05:31:26 by mreniere         ###   ########.fr       */
+/*   Updated: 2020/11/24 16:06:57 by mreniere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stdio.h>
+//#include <unistd.h>
 
-size_t		get_nbr_words(char const *str, char c)
+static size_t get_nbr_words(char const *s, char c)
 {
-	int		count;
-	int		in_word;
+	size_t count;
 
 	count = 0;
-	in_word = 0;
-	while (*str)
+	while (*s)
 	{
-		if (*str == c)
-			in_word = 0;
-		else if (in_word == 0)
-		{
-			in_word = 1;
-			count++;
-		}
-		str++;
+		while (*s && *s != c)
+			s++;
+		count++;
+		while (*s && *s == c)
+			s++;
 	}
 	return (count);
 }
 
-size_t		get_word_size(char const *str, char c, int pos)
+static size_t get_word_len(char const *s, char c)
 {
-	size_t		i;
+	size_t i;
 
 	i = 0;
-	while (str[pos] && str[pos++] == c)
-			i++;
+	while (*s && *s == c)
+		s++;
+	while (s[i] && s[i] != c)
+		i++;
 	return (i);
 }
 
-void	put_words(char **tab, char const *str, char c)
+char **ft_split(char const *s, char c)
 {
-	int		i;
-	int		j;
-	int		k;
+	size_t i;
+	size_t j;
+	size_t nbr_words;
+	char **tab;
 
 	i = -1;
-	j = 0;
-	k = 0;
-	while (str[++i])
-	{
-		if (str[i] != c)
-		{
-			if (k == 0)
-				if(!(tab[j] = malloc(
-					(get_word_size(str, c, i) + 1) * sizeof(char))))
-					return (NULL);
-			tab[j][k] = str[i];
-			tab[j][k + 1] = '\0';
-			k++;
-		}
-		else if (str[i + 1] != c && k > 0)
-		{
-			j++;
-			k = 0;
-		}
-	}
-}
-
-char		**ft_split(char const *s, char c)
-{
-	char	**tab;
-	size_t	nbr_words;
-
 	nbr_words = get_nbr_words(s, c);
-	if(!(tab = malloc((nbr_words + 1) * sizeof(char*))))
-		return(NULL);
-	put_words(tab, s, c);
-	tab[nbr_words] = NULL;
+	if (!s || !(tab = malloc(sizeof(char *) * (nbr_words + 1))))
+		return (NULL);
+	while (++i < nbr_words)
+	{
+		if (!(tab[i] = malloc(sizeof(char) * (get_word_len(s, c) + 1))))
+			return (NULL);
+		j = 0;
+		while (*s && *s == c)
+			s++;
+		while (*s && *s != c)
+			tab[i][j++] = *s++;
+		tab[i][j] = '\0';
+	}
+	tab[i] = NULL;
 	return (tab);
 }
+/*
+int main(int argc, char **argv)
+{
+	char *str = argv[1];
+	char c = 32;
+	char **ok;
+	size_t i = 0;
+
+	ok = ft_split(str, c);
+	while (ok[i])
+		printf("%s\r\n", ok[i++]);
+	return (0);
+}*/
