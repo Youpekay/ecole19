b2fc733d02b96eaf12dbe42759ab7c91148c4f5f
Youpekay <youpekay@gmail.com>
2020-11-26 01:40:38 +0100

    ok

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,13 +6,13 @@
 /*   By: mreniere <mreniere@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 15:19:39 by mreniere          #+#    #+#             */
-/*   Updated: 2020/11/26 01:36:15 by mreniere         ###   ########.fr       */
+/*   Updated: 2020/11/26 01:40:20 by mreniere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	get_len(int nbr)
+static size_t	get_len(long nbr)
 {
 	size_t	len;
 
@@ -29,25 +29,22 @@ void			ft_putnbr_fd(int n, int fd)
 	size_t	i;
 	size_t	n_len;
 	char	n_str[11];
+	long	nbr;
 
 	i = 0;
-	if(n == -2147483648)
-		ft_putstr_fd("-2147483648", fd);
-	else
+	nbr = n;
+	n_len = get_len(nbr);
+	n_str[n_len] = '\0';
+	if (nbr < 0)
 	{
-		n_len = get_len(n);
-		n_str[n_len] = '\0';
-		if (n < 0)
-		{
-			n_str[0] = '-';
-			n = -n;
-			i = 1;
-		}
-		while (n_len-- - i)
-		{
-			n_str[n_len] = (n % 10) + '0';
-			n /= 10;
-		}
-		ft_putstr_fd(n_str, fd);
+		n_str[0] = '-';
+		nbr = -nbr;
+		i = 1;
 	}
+	while (n_len-- - i)
+	{
+		n_str[n_len] = (nbr % 10) + '0';
+		nbr /= 10;
+	}
+	ft_putstr_fd(n_str, fd);
 }
