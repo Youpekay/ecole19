037c3d16c269ba569bcad36e72abde65ca469178
Youpekay <youpekay@gmail.com>
2020-11-26 01:50:02 +0100

    ok

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mreniere <mreniere@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 08:42:51 by mreniere          #+#    #+#             */
-/*   Updated: 2020/11/26 00:03:23 by mreniere         ###   ########.fr       */
+/*   Updated: 2020/11/26 01:49:31 by mreniere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,24 +29,24 @@ char	*ft_itoa(int n)
 	size_t	i;
 	size_t	n_len;
 	char	*str;
-	long	nbr;
 
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
 	i = 0;
-	nbr = n;
-	n_len = get_n_len(nbr);
+	n_len = get_n_len(n);
 	if (!(str = (char *)malloc(sizeof(char) * (n_len + 1))))
 		return (NULL);
 	str[n_len] = '\0';
-	if (nbr < 0)
+	if (n < 0)
 	{
 		str[0] = '-';
-		nbr = -nbr;
+		n = -n;
 		i = 1;
 	}
 	while (n_len-- - i)
 	{
-		str[n_len] = (nbr % 10) + '0';
-		nbr /= 10;
+		str[n_len] = (n % 10) + '0';
+		n /= 10;
 	}
 	return (str);
 }
