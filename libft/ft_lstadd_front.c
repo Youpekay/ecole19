fab9b98a45af5e7565daab7b6f24ff48569b8e57
Youpekay <youpekay@gmail.com>
2020-11-27 12:03:25 +0100

    ok

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: mreniere <mreniere@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 08:30:37 by mreniere          #+#    #+#             */
-/*   Updated: 2020/11/27 08:37:43 by mreniere         ###   ########.fr       */
+/*   Updated: 2020/11/27 12:03:12 by mreniere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!lst || !new)
 		return ;
-
 	new->next = *lst;
 	*lst = new;
 }
